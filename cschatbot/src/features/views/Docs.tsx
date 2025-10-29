1e81106d917c3040ea554fd880ff54080702eb03
sep037 <tmddms0307@dankook.ac.kr>
2025-10-30 03:18:16 +0900

    feat: 관리자 설정 페이지 추가

---
+++ cschatbot/src/features/views/Docs.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Docs = () => {
+  return (
+    <div className="centered-view">
+      <h2>도큐먼트/아티클 설정</h2>
+      <p>여기에 도큐먼트 또는 아티클 관련 내용을 표시합니다.</p>
+    </div>
+  );
+};
+
+export default Docs;
